3cc695e6b056cb383d2570a1e625224fc132d3c3
tilemachosoik <51882924+tilemachosoik@users.noreply.github.com>
2025-07-21 23:16:46 +0300

    Update ClassicsPage.tsx

--- src/pages/ClassicsPage.tsx
+++ src/pages/ClassicsPage.tsx
@@ -14,7 +14,7 @@ import speed400CaspianBlueImage from "@/assets/speed 400_my24_caspian blue_rhs.w
 import speed400PhantomBlackImage from "@/assets/speed 400_my24_phantom black_rhs.webp";
 
 import scrambler400xCarnivalRedImage from "@/assets/scrambler 400 x_my24_carnival red_rhs.webp";
-import scrambler400xMatteKhakiRedImage from "@/assets/scrambler 400 x_my24_matt khaki green_rhs.webp";
+import scrambler400xMattKhakiRedImage from "@/assets/scrambler 400 x_my24_matt khaki green_rhs.webp";
 import scrambler400xJetBlackImage from "@/assets/scrambler 400 x_my24_phantom black_rhs.webp";
 
 import scrambler400xcVanillaWhiteImage from "@/assets/scrambler-400-xc-my25-vanilla-white-rhs-1080.webp";
